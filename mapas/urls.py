1132d1f9de6a6849118fae8a521bbfb67e832912
Jearel Alcantara <jeasoft@gmail.com>
2013-04-03 02:50:23 -0400

    Adding Maps app, now has a problem with HttpResponse

---
+++ mapas/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'maps.views.index', name='index'),
+    url(r'^coords/save$', 'maps.views.coords_save'),
+    # url(r'^mapas/', include('mapas.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
---
+++ maps/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Ubicacion
+
+admin.site.register(Ubicacion)
